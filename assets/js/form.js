7eb0e9a16a00b75189f6ad0aa8225d403e00fd7c
William Lee <williamlee@Williams-MacBook-Pro.local>
2024-04-29 17:49:14 -0700

    refined the form page adding style to the form and buttons. The form now stores user input into local storage

---
+++ assets/js/form.js
@@ -0,0 +1,18 @@
+const usernameInput = document.querySelector('#username');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+const submitButton = document.querySelector('#submit');
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+  
+// create blog post object when the user clicks on the submit button
+    const blogPost = {
+      username: usernameInput.value.trim(),
+      title: titleInput.value.trim(),
+      content: contentInput.value.trim(),
+    };
+  
+// stores the input into local storage
+    localStorage.setItem('blogPost', JSON.stringify(blogPost));
+  });
